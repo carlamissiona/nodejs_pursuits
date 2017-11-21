1fd7ff6a23f26471bffba595b9414f1c3db1463f
karmicbean <missiona.carla@gmail.com>
2017-11-21 16:01:04 +0800

    Remove model

--- server/boot/autoupdate.js
+++ server/boot/autoupdate.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
 
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'assessment'];
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
 
   app.dataSources.mysqldata.autoupdate(lbTables, function(err) {
     if (err) throw err;
